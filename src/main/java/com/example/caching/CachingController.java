8827476fc3594693a82da1f1aab55d9d5b6846a7
Bepis <luqdek@gmail.com>
2024-08-01 18:25:34 +0100

    Use cacheconfig annotation to set name of cache at class level

--- src/main/java/com/example/caching/CachingController.java
+++ src/main/java/com/example/caching/CachingController.java
@@ -1,6 +1,7 @@
 package com.example.caching;
 
 import com.example.caching.models.Item;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -10,24 +11,25 @@ import java.util.MissingResourceException;
 
 @RestController
 @RequestMapping("/cache")
+@CacheConfig(cacheNames = "CachingController")
 public class CachingController {
 
     @PostMapping(path = "/create")
-    @CachePut(value = "CachingController", key = "#key")
+    @CachePut(key = "#key")
     public Item addKeyValuePairToCache(@RequestParam String key, @RequestParam String name, @RequestParam int age) throws InterruptedException {
         Thread.sleep(1000L);
         return new Item(key,name,age);
     }
 
     @PutMapping(path = "/update")
-    @CachePut(value = "CachingController", key = "#key")
+    @CachePut(key = "#key")
     public Item updateKeyValuePairInCache(@RequestParam String key, @RequestParam String name, @RequestParam int age) throws InterruptedException {
         Thread.sleep(1000L);
         return new Item(key,name,age);
     }
 
     @GetMapping(path = "/{key}")
-    @Cacheable(value = "CachingController", key = "#key")
+    @Cacheable(key = "#key")
     public Item getValueFromCache(@PathVariable String key) throws MissingResourceException {
         throw new MissingResourceException("resource not found", "Item", key);
     }
@@ -39,7 +41,7 @@ public class CachingController {
 
 
     @DeleteMapping(path = "/delete")
-    @CacheEvict(value = "CachingController", key = "#key", allEntries = false, beforeInvocation = true)
+    @CacheEvict(key = "#key", allEntries = false, beforeInvocation = true)
     public String deleteKeyInCache(@RequestParam String key) {
         return "success";
     }
