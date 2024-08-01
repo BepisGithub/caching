c30e00a694e14038004ce83fb29b508f104042ca
Bepis <luqdek@gmail.com>
2024-08-01 18:23:23 +0100

    Store slightly more complex data model in cache instead of key value pairs

--- src/main/java/com/example/caching/CachingController.java
+++ src/main/java/com/example/caching/CachingController.java
@@ -1,10 +1,12 @@
 package com.example.caching;
 
+import com.example.caching.models.Item;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.MissingResourceException;
 
 @RestController
 @RequestMapping("/cache")
@@ -12,22 +14,22 @@ public class CachingController {
 
     @PostMapping(path = "/create")
     @CachePut(value = "CachingController", key = "#key")
-    public String addKeyValuePairToCache(@RequestParam String key, @RequestParam String value) throws InterruptedException {
+    public Item addKeyValuePairToCache(@RequestParam String key, @RequestParam String name, @RequestParam int age) throws InterruptedException {
         Thread.sleep(1000L);
-        return value;
+        return new Item(key,name,age);
     }
 
     @PutMapping(path = "/update")
     @CachePut(value = "CachingController", key = "#key")
-    public String updateKeyValuePairInCache(@RequestParam String key, @RequestParam String value) throws InterruptedException {
+    public Item updateKeyValuePairInCache(@RequestParam String key, @RequestParam String name, @RequestParam int age) throws InterruptedException {
         Thread.sleep(1000L);
-        return value;
+        return new Item(key,name,age);
     }
 
     @GetMapping(path = "/{key}")
     @Cacheable(value = "CachingController", key = "#key")
-    public String getValueFromCache(@PathVariable String key) {
-        return "empty";
+    public Item getValueFromCache(@PathVariable String key) throws MissingResourceException {
+        throw new MissingResourceException("resource not found", "Item", key);
     }
 
 
---
+++ src/main/java/com/example/caching/models/Item.java
@@ -0,0 +1,12 @@
+package com.example.caching.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Item {
+    public String id;
+    public String name;
+    public int age;
+}
